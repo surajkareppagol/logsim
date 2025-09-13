c2debe60c766c3b75a1dedde0234b8986be561fd
surajkareppagol <surajkareppagol.dev@gmail.com>
2025-09-13 21:59:04 +0530

    feat: Add graph utility functions

--- include/utils.h
+++ include/utils.h
@@ -10,6 +10,10 @@
 #ifndef LOG_SIM_UTILS_H
 #define LOG_SIM_UTILS_H
 
+/*************** C Custom Headers ***************/
+
+#include "logsimtypes.h"
+
 /*************** Macros ***************/
 
 #define LOG_SIM_DEBUG 1
@@ -44,8 +48,6 @@
 
 /**************************************/
 
-/**************************************/
-
 #if LOG_SIM_FILE
 
 #define LOG_SIM_FILE_PRINT(log, fmt, ...) fprintf(log, fmt "\n", ##__VA_ARGS__)
@@ -56,7 +58,26 @@
 
 #endif
 
-/**************************************/
+/*************** Function Prototypes ***************/
+
+/**
+ * @brief Create graph node.
+ *
+ * @param label
+ * @param type
+ * @return Agnode_t*
+ */
+Agnode_t *util_create_edge(char *label, logic_block_type_t type);
+
+/**
+ * @brief Attach invisible node.
+ *
+ * @param label
+ * @param block_index
+ * @param node
+ * @return int
+ */
+int util_attach_invisible_edge(char *label, int block_index, Agnode_t *node);
 
 #endif
 
